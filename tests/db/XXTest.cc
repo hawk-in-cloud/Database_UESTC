24afe4653740debc8d7e06e8f1ffe85198963482
Lazy papee <1710620472@qq.com>
2024-07-08 20:11:09 +0800

    First clone -from teacher's exp
    from teacher's exp

---
+++ tests/db/XXTest.cc
@@ -0,0 +1,12 @@
+#include "../catch.hpp"
+#include<stdio.h>
+#include <db/schema.h>
+using namespace db;
+
+TEST_CASE("XTest")//随便改个名字
+{
+    SECTION("Papa")//随便写个名字
+    {
+        REQUIRE(true);//返回真
+    }
+}
---
+++ tests/db/x.cc
@@ -0,0 +1,13 @@
+﻿#include "../catch.hpp"
+#include<stdio.h>
+#include <db/schema.h>
+using namespace db;
+
+TEST_CASE("X")//随便改个名字
+{
+    SECTION("init")//随便写个名字
+    {
+        REQUIRE(true);//返回真
+        printf("done");
+    }
+}
